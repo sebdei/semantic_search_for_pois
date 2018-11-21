1c9c60d0f7ce2cf12bd44869f656738b7c5577f2
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-11-21 13:24:53 +0100

    assure_model_exists

--- word2vec/src/model_provider.py
+++ word2vec/src/model_provider.py
@@ -1,13 +1,31 @@
+import zipfile
 import numpy as np
+import os
+import urllib
 
-GLOVE_MODEL_DIR = 'models/glove.6B.50d.txt'
+GLOVE_MODEL_BASE_DIR = 'models/'
+GLOVE_MODEL_TXT_NAME = 'glove.6B.50d.txt'
+GLOVE_MODEL_ZIP_NAME = 'glove.6B.zip'
+GLOVE_MODEL_URL = 'http://nlp.stanford.edu/data/glove.6B.zip'
 
+def assure_model_exists():
+    if (not os.path.exists(GLOVE_MODEL_BASE_DIR + GLOVE_MODEL_ZIP_NAME) and
+    not os.path.exists(GLOVE_MODEL_BASE_DIR + GLOVE_MODEL_TXT_NAME)):
+        print('downloading GLOVE model (>800MB) ...')
+        response = urllib.urlretrieve(GLOVE_MODEL_URL, GLOVE_MODEL_BASE_DIR + GLOVE_MODEL_ZIP_NAME)
+
+    if (os.path.exists(GLOVE_MODEL_BASE_DIR + GLOVE_MODEL_ZIP_NAME) and
+    not os.path.exists(GLOVE_MODEL_BASE_DIR + GLOVE_MODEL_TXT_NAME)):
+        zip = zipfile.ZipFile(GLOVE_MODEL_BASE_DIR + GLOVE_MODEL_ZIP_NAME, 'r')
+        zip.extract(GLOVE_MODEL_TXT_NAME, GLOVE_MODEL_BASE_DIR)
 
 def load_model():
+    assure_model_exists()
+
     model = {}
     print('loading word embedding model ...')
 
-    file = open(GLOVE_MODEL_DIR,'r')
+    file = open(GLOVE_MODEL_BASE_DIR + GLOVE_MODEL_TXT_NAME,'r')
 
     for line in file:
         split_line = line.split()
