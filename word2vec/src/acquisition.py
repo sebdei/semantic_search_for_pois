02629ba3a72642626fe685777c186208d6ba661b
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-11-27 12:02:29 +0100

    acquisition

--- word2vec/src/acquisition.py
+++ word2vec/src/acquisition.py
@@ -1,5 +1,7 @@
-import urllib
+from urllib.request import urlretrieve
+
 import os
+import pandas
 
 DATA_BASE_PATH = 'data/'
 CSV_NAME = 'open_data_berlin_cultural_institutes.xlsx'
@@ -11,7 +13,15 @@ def assure_csv_file():
 
     if not os.path.exists(DATA_BASE_PATH + CSV_NAME):
         print('downloading CSV file...')
-        response = urllib.urlretrieve(CSV_URL, DATA_BASE_PATH + CSV_NAME)
+        response = urlretrieve(CSV_URL, DATA_BASE_PATH + CSV_NAME)
 
-def run():
+def perform_acqusition():
     assure_csv_file()
+
+    data_frame = pandas.read_excel(os.path.join(DATA_BASE_PATH, CSV_NAME))
+
+    for index, row in data_frame.iterrows():
+        row['Institution']
+        row['Adresse']
+        row['Lat']
+        row['Lon']
