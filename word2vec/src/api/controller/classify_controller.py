f38b57e70749bea467cf5691e0097f74311dd161
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-11-28 10:45:55 +0100

    added flask, repackaged, different word vectors

--- word2vec/src/api/controller/classify_controller.py
+++ word2vec/src/api/controller/classify_controller.py
@@ -1,10 +1,21 @@
 from flask import request
 
+from src.service import classifier_service
+
+import numpy as np
+
 def init(app):
 
     @app.route("/classify", methods = ['POST'])
     def classify():
-        input = request.json
-        
-        input['userinput']
-        return "Hello World!"
+        body = request.json
+
+        arrayOfDicts = classifier_service.classify(body['userinput'])
+
+        # work in progress
+        result = ''
+        for dict in arrayOfDicts:
+            str(dict)
+            result += str(dict)
+
+        return result
