e6d2290ea3087ec81ab104b7e7ac14b39eaee39d
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-12-03 12:24:25 +0100

    Update model_provider.py

--- word2vec/src/service/model_provider.py
+++ word2vec/src/service/model_provider.py
@@ -1,7 +1,7 @@
 import zipfile
 import numpy as np
 import os
-import urllib
+from urllib.request import urlretrieve
 
 GLOVE_MODEL_BASE_DIR = 'models'
 GLOVE_MODEL_TXT_NAME = 'glove.6B.50d.txt'
@@ -9,13 +9,14 @@ GLOVE_MODEL_ZIP_NAME = 'glove.6B.zip'
 GLOVE_MODEL_URL = 'http://nlp.stanford.edu/data/glove.6B.zip'
 
 def assure_model_exists():
-    if (not os.path.exists(os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_ZIP_NAME))
-    and not os.path.exists(os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_TXT_NAME))):
-        print('downloading GLOVE model (>800MB) ...')
-        response = urllib.urlretrieve(GLOVE_MODEL_URL, os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_ZIP_NAME))
+    if not os.path.exists(GLOVE_MODEL_BASE_DIR):
+        os.makedirs(GLOVE_MODEL_BASE_DIR)
 
-    if (os.path.exists(os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_ZIP_NAME))
-    and not os.path.exists(os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_TXT_NAME))):
+    if not (os.path.exists(os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_TXT_NAME))):
+        if (not os.path.exists(os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_ZIP_NAME))):
+            print('downloading GLOVE model (>800MB) ...')
+            response = urlretrieve(GLOVE_MODEL_URL, os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_ZIP_NAME))
+
         zip = zipfile.ZipFile(os.path.join(GLOVE_MODEL_BASE_DIR, GLOVE_MODEL_ZIP_NAME), 'r')
         zip.extract(GLOVE_MODEL_TXT_NAME, GLOVE_MODEL_BASE_DIR)
 
