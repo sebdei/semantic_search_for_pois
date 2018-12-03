f3dc8b51447cf3bdd77f7661d113a1c851ecf8a9
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-12-03 23:51:50 +0100

    style

--- word2vec/src/service/similarity_service.py
+++ word2vec/src/service/similarity_service.py
@@ -13,7 +13,7 @@ def calculate_cosine_similarity_for_article(compare_vector, article):
     return result
 
 def determine_similar_items_with_cosine_similarity(compare_vector, articles):
-    unsorted_list_of_articles_with_similarity = [calculate_cosine_similarity_for_article(compare_vector, article) for article in articles]
+    unsorted_list_of_articles_with_similarity = [ calculate_cosine_similarity_for_article(compare_vector, article) for article in articles ]
     result = sorted(unsorted_list_of_articles_with_similarity, key=lambda key: key[SIMILARITY], reverse=True)
 
     return result
