abae81f3950a7fd7426c4ebbfd48054a2cfb8c89
Unknown <t_diek09@uni-muenster.de>
2018-12-04 19:00:57 +0100

    process_functions for dataframe added

--- word2vec/src/acquisition.py
+++ word2vec/src/acquisition.py
@@ -1,5 +1,5 @@
 from urllib.request import urlretrieve
-
+from src.service import persistence_service
 import os
 import pandas
 
@@ -25,3 +25,27 @@ def perform_acqusition():
         row['Adresse']
         row['Lat']
         row['Lon']
+  #  return data_frame
+
+def split_address_format(df):
+        df['address_with_number'], df['zipcode']=df['Adresse'].str.split(',',1).str
+        df['zipcode'].replace({'[^0-9]': ''},inplace=True, regex=True)
+        df['street_number'] = df['address_with_number'].str.extract('(\\d+.?\\d*)')
+        df['street_name']=df['address_with_number']
+        df['street_name'].replace({'[\\s0-9]{2,}.*|\\d.*':''}, inplace=True, regex=True)
+
+def remove_cols(df):
+        df.drop(['Adresse', 'address_with_number'],axis=1,inplace=True)
+        df1 = df[['Institution', 'zipcode', 'street_name', 'street_number', 'Lat', 'Lon']]
+ 
+def reorder_cols(df):
+    df1=df[['Institution', 'zipcode', 'street_name', 'street_number', 'Lat', 'Lon']]
+    df1.rename(index=str, columns={"Institution": "institution", "zipcode":"zipcode",'street_number':'street_number', 'street_name':'street_name',"Lat": "lat", "Lon":"lon"},inplace=True)
+    return df1
+ 
+def process_data_frame():
+        df=perform_acqusition()
+        df.usecols(0,1,2,3)
+        return df
+
+
