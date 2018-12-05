5ce84a22756933b1173bf4876a5d1328edc485cd
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-12-05 16:09:14 +0100

    update wiki search

--- word2vec/src/service/wikipedia_search.py
+++ word2vec/src/service/wikipedia_search.py
@@ -1,13 +1,19 @@
 import wikipedia
 import string
+import pandas as pd
+
+from .persistence_service import get_all_points_of_interests
 
 def clean_query(query):
     exclude = set(string.punctuation)
     no_punctuation = ''.join(ch for ch in query if ch not in exclude)
+
     return no_punctuation
 
 def get_wikipedia_text(query):
     search_query = clean_query(query)
+    print('lookup wikipedia for searchquery >>' + search_query + '<<')
+
     results_with_berlin = wikipedia.search(search_query + " Berlin")
     results_without_berlin = wikipedia.search(search_query)
 
@@ -21,12 +27,15 @@ def get_wikipedia_text(query):
     else:
         # prefer result with "Berlin"
         wiki_page = results_with_berlin[0]
-    
-    wiki_text = wikipedia.page(title=wiki_page).content
-    print(query + "\n\t" + wiki_page)
-    
-    return (wiki_page, wiki_text)
 
-# names = open("names.txt")
-# for line in names.read().split('\n'):
-#     print(get_wikipedia_text(line))
+    wiki_text = wikipedia.page(title = wiki_page).content
+
+    return wiki_text
+
+def perform_wikipedia_loopup():
+    columns = ['id', 'name', 'street_name', 'street_number', 'zip_code', 'long', 'lat', 'opening_hours', 'word2vec']
+    dataframe = pd.DataFrame(get_all_points_of_interests(), columns = columns)[['id', 'name']]
+
+    dataframe['text'] = dataframe.apply(lambda row: get_wikipedia_text(row['name']), axis = 1)
+
+    return dataframe
