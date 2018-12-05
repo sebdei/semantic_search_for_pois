d049b5830736443f2cb1ace05e811ee14fab5776
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-12-05 20:51:24 +0100

    cleanup

--- word2vec/src/service/similarity_service.py
+++ word2vec/src/service/similarity_service.py
@@ -1,8 +1,4 @@
-import numpy as np
 from scipy.spatial import distance
-import random
-
-SIMILARITY = 'similarity'
 
 def calculate_cosine_similarity_for_article(compare_vector, article_vector):
     cosine_distance = distance.cosine(compare_vector, article_vector)
@@ -11,4 +7,4 @@ def calculate_cosine_similarity_for_article(compare_vector, article_vector):
 def determine_similar_items_with_cosine_similarity(compare_vector, articles_data_frame):
     articles_data_frame['similarity'] = articles_data_frame.apply(lambda row: calculate_cosine_similarity_for_article(compare_vector, row['weighted_word2vec']), axis = 1)
 
-    return articles_data_frame.sort_values(by=['similarity'], ascending=False)
+    return articles_data_frame.sort_values(by = ['similarity'], ascending = False)
