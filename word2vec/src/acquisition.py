7252836d34556419a006d285af32b18734068f21
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-12-05 11:19:45 +0100

    improvements for acqusition

--- word2vec/src/acquisition.py
+++ word2vec/src/acquisition.py
@@ -1,7 +1,7 @@
 from urllib.request import urlretrieve
-from src.service import persistence_service
+# from src.service import persistence_service
 import os
-import pandas
+import pandas as pd
 import re
 
 DATA_BASE_PATH = 'data/'
@@ -17,42 +17,43 @@ def assure_csv_file():
         response = urlretrieve(CSV_URL, DATA_BASE_PATH + CSV_NAME)
 
 def perform_acqusition():
+    print('perform_acqusition')
     assure_csv_file()
 
-    data_frame = pandas.read_excel(os.path.join(DATA_BASE_PATH, CSV_NAME))
-    data_frame2= process_data_frame(data_frame)
-    load_data_frame_into_postgres(data_frame2)
+    source_data_frame = pd.read_excel(os.path.join(DATA_BASE_PATH, CSV_NAME))
+    processed_data_frame = process_data_frame(source_data_frame)
+
+    load_data_frame_into_postgres(processed_data_frame)
 
 def split_address(address):
-        address_with_number, zip_code = (address.split(',',1)+[None])[:2]
-        street_name=re.sub('[\s0-9]{2,}.*|\d.*','',address_with_number)
-        street_number=re.search(r'(\d+.?\d*.?)',address_with_number)
-        if street_number is None:
-                street_number=None
-        else:
-                street_number=street_number.group(0).strip()
-        if zip_code is None:
-                zip_code=None
-        else:
-                zip_code=re.sub('[^0-9]', '',zip_code)
-                zip_code=zip_code.strip()
-        street_name=street_name.strip()
-        return street_name, street_number, zip_code
+    address_with_number, zip_code = (address.split(',',1)+[None])[:2]
+    street_name = re.sub('[\s0-9]{2,}.*|\d.*','', address_with_number)
+    street_number = re.search(r'(\d+.?\d*.?)', address_with_number)
+
+    if street_number is not None:
+        street_number = street_number.group(0).strip()
+
+    if zip_code is not None:
+        zip_code = re.sub('[^0-9]', '', zip_code)
+        zip_code = zip_code.strip()
+
+    if street_name is not None:
+        street_name = street_name.strip()
+
+    return street_name, street_number, zip_code
 
 def process_data_frame(df):
-        columns=['name', 'street_name', 'street_number', 'zip_code', 'long','lat']
-        data_frame2=pandas.DataFrame(columns=columns)
-        for index, row in df.iterrows():
-                street_name,street_number,zip_code=split_address(row['Adresse'])
-                new_row=[row['Institution'],street_name,street_number,zip_code,row['Lon'],row['Lat']]
-                data_frame3=pandas.DataFrame([new_row], columns=columns)
-                data_frame2=data_frame2.append(data_frame3, ignore_index=True)
-        return data_frame2
+    columns = ['name', 'street_name', 'street_number', 'zip_code', 'long','lat']
+    processed_data_frame = pd.DataFrame(columns=columns)
+
+    for index, row in df.iterrows():
+        street_name, street_number, zip_code = split_address(row['Adresse'])
+        new_panda_row = pd.Series([ row['Institution'], street_name, street_number, zip_code, row['Lon'], row['Lat'] ], columns)
+
+        processed_data_frame =  processed_data_frame.append([new_panda_row], ignore_index=True)
+
+    return processed_data_frame
 
 def load_data_frame_into_postgres(df):
-        for index, row in df.iterrows():
-                persistence_service.insert_into_points_of_interests(row['name'], row['street_name'], row['street_number'], row['zip_code'], row['long'], row['lat'], None, None)
-
-
-
-                
+    for index, row in df.iterrows():
+        persistence_service.insert_into_points_of_interests(row['name'], row['street_name'], row['street_number'], row['zip_code'], row['long'], row['lat'], None, None)
