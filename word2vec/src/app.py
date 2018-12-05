4b1383cb2502c584d4a842916605d49dc3378c58
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-12-05 20:51:00 +0100

    Update app.py

--- word2vec/src/app.py
+++ word2vec/src/app.py
@@ -9,11 +9,9 @@ from src.service import model_provider
 from src.service import similarity_service
 from src.service import word_embedding_service
 
-
 import pandas as pd
 
 def run():
-    # persistence_service.create_initial_schema()
     # perform_acqusition()
 
     dataframe = wikipedia_search.perform_wikipedia_lookup()
@@ -22,13 +20,19 @@ def run():
     for index, row in weighted_word_matrix.iterrows():
         persistence_service.update_weighted_word2vec_by_id(index, row.get_values().tolist())
 
-
     # -- similarity --
     # columns = ['id', 'name', 'street_name', 'street_number', 'zip_code', 'long', 'lat', 'opening_hours', 'weighted_word2vec']
     # dataframe = pd.DataFrame(persistence_service.get_all_points_of_interests(), columns = columns)[['id', 'name', 'weighted_word2vec']]
     #
     # model = model_provider.provide_glove_model()
-    # userinputarray = word_embedding_service.calculate_mean_vector_of_word_embeddings_for_text('Donald trump has stated to visit Berlin and its favourit places. He likes Jewish art and german history museums.', model)
+    #
+    #
+    # userinput = 'art museum'
+    # cleanedinput = word2vec_preprocess.clean_article(userinput, model)
+    #
+    # print(cleanedinput)
+    #
+    # userinputarray = word_embedding_service.calculate_mean_vector_of_word_embeddings_for_text(cleanedinput, model)
     #
     # result = similarity_service.determine_similar_items_with_cosine_similarity(userinputarray, dataframe)
     #
