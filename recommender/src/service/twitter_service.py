40d431b41b94542d0a49153c03c4e4dde41a9eee
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2019-01-19 00:38:32 +0100

    get full twitter texts when querying by username
    see https://github.com/tweepy/tweepy/issues/974#issuecomment-360228403

--- recommender/src/service/twitter_service.py
+++ recommender/src/service/twitter_service.py
@@ -25,7 +25,7 @@ def search_for_user_ids(q):
 
 def get_recent_tweets_of_user_by_name(name):
     tweet_list=list()
-    user_timeline=api.user_timeline(name)
+    user_timeline=api.user_timeline(name, tweet_mode='extended')
     for i, val in enumerate(user_timeline):
-        tweet_list.append(user_timeline[i].text)
+        tweet_list.append(user_timeline[i].full_text)
     return tweet_list
