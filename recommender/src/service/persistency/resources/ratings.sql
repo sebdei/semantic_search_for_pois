372a32438ad1801a1bf4e350a1572209e516ed57
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2019-01-19 01:00:53 +0100

    Add users and ratings schemata
    and optimize some parts in the persistence_service

---
+++ recommender/src/service/persistency/resources/ratings.sql
@@ -0,0 +1,10 @@
+-- CREATE script for ratings table
+-- 
+
+CREATE TABLE IF NOT EXISTS ratings (
+    u_id integer REFERENCES users(id) ON DELETE CASCADE,
+    poi_id integer REFERENCES points_of_interests(id) ON DELETE CASCADE,
+    rating smallint,
+    
+    PRIMARY KEY (u_id, poi_id)
+);
