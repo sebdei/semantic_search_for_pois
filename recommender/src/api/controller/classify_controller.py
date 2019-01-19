50c8cdaca7cee92c500bda5ccd9466ed794385d5
Sebastian Deisel <sebdei@users.noreply.github.com>
2019-01-19 18:30:36 +0100

    Update classify_controller.py

--- recommender/src/api/controller/classify_controller.py
+++ recommender/src/api/controller/classify_controller.py
@@ -5,9 +5,9 @@ import json
 # from src.service import classifier_service
 
 from src.service.persistency import pandas_persistence_service
+from src.service.persistency import persistence_service
 
 def init(app):
-
     @app.route("/classify", methods = ['POST'])
     def classify():
         body = request.json
@@ -19,12 +19,14 @@ def init(app):
     @app.route('/points_of_interests/<id>')
     def get(id):
         assert id == request.view_args['id']
-        result = pandas_persistence_service.get_points_of_interests_by_id_as_df(id)
+        poi_data_frame = pandas_persistence_service.get_points_of_interests_by_id_as_df(id)
 
-        return result.reset_index().to_json(orient='records')
+        poi_data_frame['text'] = poi_data_frame.apply(lambda row: persistence_service.get_text_for_poi(row.id)[1], axis=1)
+
+        return poi_data_frame.reset_index().to_json(orient='records')
 
     @app.route('/points_of_interests/')
     def get_all():
-        result = pandas_persistence_service.get_all_points_of_interests_as_df()
+        poi_data_frame = pandas_persistence_service.get_all_points_of_interests_as_df()
 
         return result.reset_index().to_json(orient='records')
