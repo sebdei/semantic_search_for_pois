f5dddf94566dffe0b9bad39578c343bfa9a22295
Sebastian Deisel <sebdei@users.noreply.github.com>
2019-01-19 21:53:42 +0100

    extracted add source column to df

--- recommender/src/api/controller/points_of_interests.py
+++ recommender/src/api/controller/points_of_interests.py
@@ -7,13 +7,18 @@ from src.service import classifier_service
 from src.service.persistency import pandas_persistence_service
 from src.service.persistency import persistence_service
 
+def add_source_column_to_data_frame(poi_data_frame):
+    poi_data_frame['source'] = poi_data_frame.apply(lambda row: persistence_service.get_text_for_poi(row.id), axis=1)
+
+    return poi_data_frame
+
 def init(app):
     @app.route("/classify", methods = ['POST'])
     def classify():
         body = request.json
 
         poi_data_frame = classifier_service.classify(body['query']).reset_index()
-        poi_data_frame['source'] = poi_data_frame.apply(lambda row: persistence_service.get_text_for_poi(row.id), axis=1)
+        poi_data_frame = add_source_column_to_data_frame(poi_data_frame)
 
         return poi_data_frame.reset_index().to_json(orient='records')
 
@@ -22,7 +27,7 @@ def init(app):
         assert id == request.view_args['id']
         poi_data_frame = pandas_persistence_service.get_points_of_interests_by_id_as_df(id).reset_index()
 
-        poi_data_frame['source'] = poi_data_frame.apply(lambda row: persistence_service.get_text_for_poi(row.id), axis=1)
+        poi_data_frame = add_source_column_to_data_frame(poi_data_frame)
 
         return poi_data_frame.to_json(orient='records')
 
@@ -30,7 +35,7 @@ def init(app):
     def get_all():
         poi_data_frame = pandas_persistence_service.get_all_points_of_interests_as_df().reset_index()[10:100]
 
-        poi_data_frame['source'] = poi_data_frame.apply(lambda row: persistence_service.get_text_for_poi(row.id), axis=1)
+        poi_data_frame = add_source_column_to_data_frame(poi_data_frame)
 
         return poi_data_frame.to_json(orient='records')
 
