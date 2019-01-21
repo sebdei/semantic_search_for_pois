657ef56baa8be983c243755d192e0e2c9477b2c2
Jens Mechelhoff <j_mech01@uni-muenster.de>
2019-01-21 18:30:23 +0100

    Flip between recommendation type implemented in flask

--- recommender/src/api/controller/points_of_interests.py
+++ recommender/src/api/controller/points_of_interests.py
@@ -6,6 +6,8 @@ from src.service import classifier_service
 
 from src.service.persistency import pandas_persistence_service
 from src.service.persistency import persistence_service
+from src.service.collaborative_filtering import user2user_recommender
+from src.service.collaborative_filtering import filterWeather, filterLocation
 
 def add_source_column_to_data_frame(poi_data_frame):
     poi_data_frame['source'] = poi_data_frame.apply(lambda row: persistence_service.get_text_for_poi(row.id), axis=1)
@@ -21,6 +23,42 @@ def init(app):
         poi_data_frame = add_source_column_to_data_frame(poi_data_frame)
 
         return poi_data_frame.reset_index().to_json(orient='records')
+
+    @app.route("/classify2", methods = ['POST'])
+    def classify_contentBased_collaborativeFiltering():
+        body = request.json
+
+        # Step 1: Copy parameters to local variables
+        currentUserId = body['u_id']
+        weatherApi_bool = body['weatherAPI']
+        forceBadWeather_bool = body['forceBadWeather']
+        user_lat = body['latitude']
+        user_lng = body['longitude']
+        radius = body['radius']
+
+        # Step 2: Decide wether recommendation is content-based or collaborative filtering
+        if persistence_service.get_recommenderType(currentUserId) == "collaborativeFiltering" and \
+            user2user_recommender.eval(currentUserId) < 0.001:
+                # COLLABORATIVE FILTERING
+
+                recommendations = user2user_recommender.getRecommendationsForUser(currentUserId)
+                usersCurrentLocation = {'lat':user_lat, 'lng':user_lng} #some place in berlin
+
+                # apply filter: weather
+                recommendations = filterWeather.filterOnWeather(usersCurrentLocation, recommendations, weatherApi_bool, forceBadWeather_bool)
+
+                # applyfilter: location
+                recommendations = filterLocation.filterOnLocation(usersCurrentLocation, recommendations, radius)
+
+                # return results
+                return recommendations.reset_index().to_json(orient='records')
+
+        else:
+            #CONTENT-BASED RECOMMENDATION
+            poi_data_frame = classifier_service.classify(body['query']).reset_index()
+            poi_data_frame = add_source_column_to_data_frame(poi_data_frame)
+            return poi_data_frame.reset_index().to_json(orient='records')
+
 
     @app.route('/points_of_interests/<id>')
     def get(id):
