99a9af8e177c18890b41776f59d095e8e873fadc
Sebastian Deisel <sebdei@users.noreply.github.com>
2019-01-22 22:11:59 +0100

    app flask rdy

--- recommender/src/app.py
+++ recommender/src/app.py
@@ -1,30 +1,30 @@
-from src.service.embedding_preprocess import embedding_preprocess
-from src.service.embedding_preprocess import visitBerlin_search
-from src.service.acquisition.open_street_map import import_osm_points_of_interest
-from src.service.acquisition.open_data_berlin import import_odb_points_of_interest
-from src.service.schema_fusion import import_into_poi_table
-from src.service.restore_default import restore_default_database
-from src.service.collaborative_filtering import user2user_recommender
-from src.service.collaborative_filtering import filterWeather, filterLocation
-from src.service.persistency.persistence_service import get_text_for_poi, create_schemata
+# from src.service.embedding_preprocess import embedding_preprocess
+# from src.service.embedding_preprocess import visitBerlin_search
+# from src.service.acquisition.open_street_map import import_osm_points_of_interest
+# from src.service.acquisition.open_data_berlin import import_odb_points_of_interest
+# from src.service.schema_fusion import import_into_poi_table
+# from src.service.restore_default import restore_default_database
+# from src.service.collaborative_filtering import user2user_recommender
+# from src.service.collaborative_filtering import filterWeather, filterLocation
+# from src.service.persistency.persistence_service import get_text_for_poi, create_schemata
 
 # import pandas as pd
 
-# from src.api import flask
+from src.api import flask
 
 def run():
-    # flask.bindRoutes()
+    flask.bindRoutes()
 
+    # drop all tables and import default data
 
     # do a manual import of from source (ODB/OSM/Wikipedia/VisitBerlin)
     # create_schemata()
     # import_odb_points_of_interest()
     # import_osm_points_of_interest()
     # import_into_poi_table()
+    # restore_default_database()
 
 
-    # drop all tables and import default data
-    #restore_default_database()
 
     ### Tests ###
 
@@ -33,9 +33,9 @@ def run():
     # print(result_dict)
 
     # test collaborative filtering
-    evaluationForRecommendation = user2user_recommender.eval(1)
-    recommendations = user2user_recommender.getRecommendationsForUser(1)
-    usersCurrentLocation = {'lat':52.520008, 'lng':13.404954} #some place in berlin
+    # evaluationForRecommendation = user2user_recommender.eval(1)
+    # recommendations = user2user_recommender.getRecommendationsForUser(1)
+    # usersCurrentLocation = {'lat':52.520008, 'lng':13.404954} #some place in berlin
     #filterWeather.filterOnWeather(usersCurrentLocation, recommendations, True, False)
     #filterLocation.filterOnLocation(usersCurrentLocation, recommendations, 6)
 
