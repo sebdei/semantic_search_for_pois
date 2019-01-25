0929a1023f8b3786a0f4193ea882c1cde2243f52
Sebastian Deisel <sebdei@users.noreply.github.com>
2019-01-25 17:30:39 +0100

    fix

--- recommender/src/web/flask.py
+++ recommender/src/web/flask.py
@@ -4,19 +4,18 @@ from flask_cors import CORS
 from .controller import points_of_interests_controller
 from .controller import users_controller
 
-def bind_index_render_route():
+def bind_index_render_route(app):
     @app.route('/')
     def index():
         return render_template("index.html")
 
 def bindRoutes():
-    app = Flask(__name__, static_folder = "./dist",
-            template_folder = "./dist")
+    app = Flask(__name__, static_folder = "./dist", template_folder = "./dist")
     CORS(app)
 
     print('binding Routes...')
 
-    bind_index_render_route()
+    bind_index_render_route(app)
 
     # API
     points_of_interests_controller.init(app)
