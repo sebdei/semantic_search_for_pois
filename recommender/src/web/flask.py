b3b74893735f17c1bd715db16c0ab37cc143b895
Sebastian Deisel <sebdei@users.noreply.github.com>
2019-01-25 17:22:13 +0100

    repackaging

--- recommender/src/web/flask.py
+++ recommender/src/web/flask.py
@@ -1,15 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 from .controller import points_of_interests_controller
 from .controller import users_controller
 
+def bind_index_render_route():
+    @app.route('/')
+    def index():
+        return render_template("index.html")
+
 def bindRoutes():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder = "./dist",
+            template_folder = "./dist")
     CORS(app)
 
     print('binding Routes...')
 
+    bind_index_render_route()
+
+    # API
     points_of_interests_controller.init(app)
     users_controller.init(app)
 
