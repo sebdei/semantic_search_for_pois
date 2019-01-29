6d3aac441df365e4140cf6ec38f9fd84f0f7773b
Sebastian Deisel <sebdei@users.noreply.github.com>
2019-01-29 16:23:48 +0100

    Update Welcome.vue

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -20,6 +20,32 @@
         </div>
       </transition>
     </div>
+    <div class="footer">
+      <div class="presented">
+        <div >
+          <a href="https://www.uni-muenster.de/de/">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/7/7f/Logo_WWU_M%C3%BCnster.svg" />
+          </a>
+        </div>
+        <div >
+          <a href="https://www.wi.uni-muenster.de/department/dbis">
+            <img src="http://www.ercis-launchpad.de/images/logo_dbis.png" />
+          </a>
+        </div>
+      </div>
+
+      <div class="thanks-source">
+        <a href="https://daten.berlin.de/">
+          <img id="open-berlin-img" src="https://www.berlin.de/css/berlin_de/echo/images/logo_berlin_de.svg">
+        </a>
+        <a href="https://www.openstreetmap.org">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Openstreetmap_logo.svg/1200px-Openstreetmap_logo.svg.png">
+        </a>
+        <a href="https://wikipedia.org">
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1920px-Wikipedia-logo-v2.svg.png">
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -73,7 +99,7 @@ export default {
 
 .b-card {
   position: relative;
-  height: 300px;
+  height: 360px;
   cursor: pointer;
   flex-direction: row;
   width: 100%;
@@ -97,4 +123,34 @@ export default {
 .twitter-enter-to {
   width: 100%;
 }
+
+.footer {
+  padding: 40px;
+}
+
+.presented {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+}
+
+.presented img {
+  width: 150px;
+  margin: 0
+}
+
+.thanks-source {
+  display: flex;
+  justify-content: space-between;
+}
+
+.thanks-source img {
+  margin: 0 5px;
+  width: 60px;
+}
+
+#open-berlin-img {
+  width: 70px;
+}
+
 </style>
